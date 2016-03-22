dea6976199e0d7acd3bed0d70a92fde50fb25bfb
SergeyKhval <sergeykhval@gmail.com>
2016-03-22 14:28:47 +0300

    Chore: change bower.json to be correctly used by wiredep

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,8 +33,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
-      'bower_components/bootstrap-material-design/dist/js/material.js',
-      'bower_components/bootstrap-material-design/dist/js/ripples.js',
+      'bower_components/bootstrap-material-design/scripts/material.js',
+      'bower_components/bootstrap-material-design/scripts/ripples.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
