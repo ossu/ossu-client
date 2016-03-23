cfdfcf09d0515b475b91016f05ad9fce28028868
SergeyKhval <sergeykhval@gmail.com>
2016-03-23 12:40:55 +0300

    Fix #2: refactor curriculum with proper ng-repeat

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,39 @@ angular.module('ossuClientApp')
 
     $scope.courses = null;
 
+    $scope.categories = [
+      'Introduction to Computer Science',
+      'Math (Mathematical Thinking)',
+      'Program Design',
+      'Math (Discrete Math)',
+      'Algorithms',
+      'Programming Paradigms',
+      'Software Testing',
+      'Math (Calculus)',
+      'Software Architecture',
+      'Theory',
+      'Software Engineering',
+      'Math (Probability)',
+      'Computer Architecture',
+      'Operating Systems',
+      'Computer Networks',
+      'Databases',
+      'Cloud Computing',
+      'Math (Linear Algebra)',
+      'Cryptography',
+      'Security',
+      'Compilers',
+      'Parallel Computing',
+      'UX Design',
+      'Computer Graphics',
+      'Artificial Intelligence',
+      'Machine Learning',
+      'Natural Language Processing',
+      'Big Data',
+      'Data Mining',
+      'Internet of Things'
+    ];
+
     Course.getCourses().$loaded().then(function(data){
       $scope.courses = data;
     })
