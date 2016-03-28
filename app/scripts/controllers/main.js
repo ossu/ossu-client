09b50b463f9e629c14ec2ece1637c101aaed5794
SergeyKhval <sergeykhval@gmail.com>
2016-03-28 16:33:25 +0300

    refactor: remove unnecessary logic from MainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,51 +8,6 @@
  * Controller of the ossuClientApp
  */
 angular.module('ossuClientApp')
-  .controller('MainCtrl', function ($scope, $location, User) {
-    function redirect() {
-      $location.path('/account');
-    }
-
-    function showError(err) {
-      $scope.err = err;
-    }
-
-    $scope.categories = [
-      'Introduction to Computer Science',
-      'Math (Mathematical Thinking)',
-      'Program Design',
-      'Math (Discrete Math)',
-      'Algorithms',
-      'Programming Paradigms',
-      'Software Testing',
-      'Math (Calculus)',
-      'Software Architecture',
-      'Theory',
-      'Software Engineering',
-      'Math (Probability)',
-      'Computer Architecture',
-      'Operating Systems',
-      'Computer Networks',
-      'Databases',
-      'Cloud Computing',
-      'Math (Linear Algebra)',
-      'Cryptography',
-      'Security',
-      'Compilers',
-      'Parallel Computing',
-      'UX Design',
-      'Computer Graphics',
-      'Artificial Intelligence',
-      'Machine Learning',
-      'Natural Language Processing',
-      'Big Data',
-      'Data Mining',
-      'Internet of Things'
-    ];
-
-    $scope.githubLogin = function () {
-      $scope.err = null;
-      User.githubLogin().then(redirect, showError);
-    };
+  .controller('MainCtrl', function () {
 
   });
